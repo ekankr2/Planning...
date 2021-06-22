187c61e93840ab961e1ffb0e9f42b272328cdec8
ekank <ekankr2@naver.com>
2021-06-22 10:32:55 +0900

    file moving test

--- javascript/IkhwanIm/project/src/main/java/com/example/project/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.service;
-
-import com.example.project.entity.Login;
-import org.springframework.stereotype.Service;
-
-public interface LoginService {
-    public void register(Login login) throws Exception;
-}
